b9e2efc0c513d8b28f5042bac555f957d4fa5294
dinsarendev <dinsarendev@gmail.com>
2026-08-29 11:28:35 +0700

    feat: Project Structure - Local Storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:post_mobile_application/routes/app_route_name.dart';
 import 'package:post_mobile_application/routes/app_routes.dart';
 
-void main() {
+Future<void> main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -12,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: AppRouteName.splash,
       getPages: AppRoutes.getAllRoutes(),
       title: 'Post Mobile Application',
--- lib/modules/auth/login/view/login_view.dart
+++ lib/modules/auth/login/view/login_view.dart
@@ -9,9 +9,45 @@ class LoginView extends GetView<LoginView> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: 35,
-          horizontal: 16
+        padding: EdgeInsets.symmetric(vertical: 35, horizontal: 16),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(top: 45),
+              child: Text(
+                textAlign: TextAlign.center,
+                "Login",
+                style: TextStyle(color: Colors.black, fontSize: 24),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Colors.cyan),
+                  ),
+                  hintText: "Username",
+                  labelText: "Username",
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 2, color: Colors.cyan),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(),
+                  border: OutlineInputBorder(),
+                  hintText: "Password",
+                  labelText: "Password",
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
